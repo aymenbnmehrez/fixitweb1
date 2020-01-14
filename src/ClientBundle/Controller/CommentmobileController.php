62240337c6b1ca87b45a5909f3437f0aa816edff
aminbousbih <medamin.bousbih@gmail.com>
2020-01-14 16:12:52 +0100

    test

---
+++ src/ClientBundle/Controller/CommentmobileController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace ClientBundle\Controller;
+use AppBundle\Entity\Comments;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use UserBundle\Entity\User;
+use AppBundle\Entity\Post;
+use AppBundle\Form\PostType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+
+
+
+class CommentmobileController extends Controller
+{
+   public function affichercomMobileAction()
+   {
+       $tab = $this->getDoctrine()->getManager()->getRepository(Comments::class)->findAll();
+
+       $encoders = array(new XmlEncoder(), new JsonEncoder());
+
+       $normalizer = new ObjectNormalizer();
+       $normalizer->setCircularReferenceLimit(2);
+// Add Circular reference handler
+       $normalizer->setCircularReferenceHandler(function ($object) {
+           return $object;
+       });
+       $normalizers = array($normalizer);
+       $serializer = new Serializer($normalizers, $encoders);
+//    $serializer=new Serializer([new ObjectNormalizer()]);
+       $formatted=$serializer->normalize($tab);
+       return new JsonResponse($formatted);
+   }
+
+
+
+
+    public function ajoutercomMobileAction(Request $request)
+    {   $comment = new Comments();
+        $em = $this->getDoctrine()->getManager();
+        $comment->setComment($request->get('comment'));
+        $em->persist($comment);
+        $em->flush();
+        $serializer=new Serializer([new ObjectNormalizer()]);
+        $formatted=$serializer->normalize($comment);
+        return new JsonResponse($formatted);
+    }
+}
